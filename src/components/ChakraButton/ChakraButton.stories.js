921d3f9463f7391dc604326af5f37a17b5c68779
Yukyung Kim / Sally <rladbrud12@naver.com>
2023-12-08 00:55:09 +0900

    feat : args

--- src/components/ChakraButton/ChakraButton.stories.js
+++ src/components/ChakraButton/ChakraButton.stories.js
@@ -3,7 +3,24 @@ import { Button } from "@chakra-ui/react";
 export default {
   title: "Chakra/Button",
   component: Button,
+  argTypes: {
+    children: {
+      control: "text",
+    },
+    colorScheme: {
+      control: "text",
+    },
+  },
 };
 
-export const Success = () => <Button colorScheme="green">Success</Button>;
+export const Success = {
+  render: (args) => {
+    return <Button {...args} onClick={() => console.log("clicked")} />;
+  },
+  args: {
+    children: "success",
+    colorScheme: "green",
+  },
+};
+
 export const Danger = () => <Button colorScheme="red">Danger</Button>;
